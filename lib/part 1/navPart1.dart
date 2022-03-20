41da31da33d525213af8d3bfeaddeda5a941f68a
dafisatria <satria1002@gmail.com>
2022-03-20 14:17:26 +0700

    second commit for part 1

---
+++ lib/part 1/navPart1.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class NavPart1 extends StatelessWidget {
+  const NavPart1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main screen'),
+      ),
+      body: GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return const DetailScreen();
+            }));
+          },
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.network(
+              'https://picsum.photos/250?image=9',
+            ),
+          )),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+            child: Hero(
+          tag: 'imageHero',
+          child: Image.network(
+            'https://picsum.photos/250?image=9',
+          ),
+        )),
+      ),
+    );
+  }
+}
---
+++ lib/part 1/screen.dart
@@ -0,0 +1,1 @@
+export 'navPart1.dart';
