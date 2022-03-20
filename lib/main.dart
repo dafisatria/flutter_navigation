6756f8833cff3e2f5c8d9b2208f4eddec9db3034
dafisatria <satria1002@gmail.com>
2022-03-20 15:28:46 +0700

    fifth commit for part 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_navigation/navigations/screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'navigations/screen.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Named Routes Demo',
-        // Start the app with the "/" named route. In this case, the app starts
-        // on the FirstScreen widget.
-        initialRoute: '/',
-        routes: {
-          // When navigating to the "/" route, build the FirstScreen widget.
-          '/': (context) => const FirstScreen(),
-          // When navigating to the "/second" route, build the SecondScreen widget.
-          '/second': (context) => const SecondScreen(),
-        },
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        ExtractArgumentsScreen.routeName: (context) =>
+            const ExtractArgumentsScreen(),
+      },
+      // Provide a function to handle named routes.
+      // Use this function to identify the named
+      // route being pushed, and create the correct
+      // Screen.
+      onGenerateRoute: (settings) {
+        // If you push the PassArguments route
+        if (settings.name == PassArgumentsScreen.routeName) {
+          // Cast the arguments to the correct
+          // type: ScreenArguments.
+          final args = settings.arguments as ScreenArguments;
+
+          // Then, extract the required data from
+          // the arguments and pass the data to the
+          // correct screen.
+          return MaterialPageRoute(
+            builder: (context) {
+              return PassArgumentsScreen(
+                title: args.title,
+                message: args.message,
+              );
+            },
+          );
+        }
+        // The code only supports
+        // PassArgumentsScreen.routeName right now.
+        // Other values need to be implemented if we
+        // add them. The assertion here will help remind
+        // us of that higher up in the call stack, since
+        // this assertion would otherwise fire somewhere
+        // in the framework.
+        assert(false, 'Need to implement ${settings.name}');
+        return null;
+      },
+      title: 'Navigation with Arguments',
+      home: const HomeScreen(),
+    );
+  }
 }
--- lib/navigations/screen.dart
+++ lib/navigations/screen.dart
@@ -1,3 +1,4 @@
 export 'navPart1.dart';
 export 'navPart2.dart';
 export 'navPart3.dart';
+export 'navPart4.dart';
